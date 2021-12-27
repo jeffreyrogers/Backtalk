b5e6b6731651d3e9928d05835583005db3c6a904
Jeff Rogers <jeffreyrogers27@gmail.com>
2021-12-26 17:02:49 -0800

    Implement POST /comment/<slug> api call
    Logic is correct, but csrf protection needs to be updated to allow the
    POST to succeed.

--- internal/handlers/comments.go
+++ internal/handlers/comments.go
@@ -3,11 +3,43 @@ package handlers
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jeffreyrogers/backtalk/internal/globals"
+	"github.com/jeffreyrogers/backtalk/internal/sqlc"
 )
 
 func CreateComment(w http.ResponseWriter, r *http.Request) {
+	// get slug from
+	slug := chi.URLParam(r, "slug")
+	if slug == "" {
+		w.WriteHeader(400)
+		log.Println("No slug provided to CreateComment")
+		return
+	}
+
+	author := r.FormValue("author")
+	if author == "" {
+		w.WriteHeader(400)
+		log.Println("No author provided to CreateComment")
+		return
+	}
+
+	comment := r.FormValue("comment")
+	if comment == "" {
+		w.WriteHeader(400)
+		log.Println("No comment provided to CreateComment")
+		return
+	}
+
+	_, err := globals.Queries.CreateComment(globals.Ctx, sqlc.CreateCommentParams{slug, author, comment})
+	if err != nil {
+		w.WriteHeader(503)
+		log.Println("Could not create comment")
+		return
+	}
+
 	w.WriteHeader(200)
-	w.Write([]byte("Created Comment"))
 	log.Println("Created comment")
 }
 
